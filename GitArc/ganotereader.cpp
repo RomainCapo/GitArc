a7bdb0df9ccc77f2bce3c26b4c82c519d8d44b89
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-01-12 13:48:20 +0100

    commentaire

--- GitArc/ganotereader.cpp
+++ GitArc/ganotereader.cpp
@@ -1,5 +1,6 @@
 #include "ganotereader.h"
 #include "constants.h"
+#include "gasettings.h"
 #include <QFile>
 #include <QList>
 #include <QDebug>
@@ -26,8 +27,8 @@ void GANoteReader::readPartition()
     this->readCSVNote();
 
     this->noteTimer = new QTimer(this);
-    // FIXME : fréquence d'échantillonage pas en magique
     noteTimer->setInterval(1000);
+    //qDebug() << 500 * (GASettings::difficulty + 1);
     this->connect(noteTimer, &QTimer::timeout, this, &GANoteReader::getNotes);
     noteTimer->start();
 }
@@ -44,17 +45,20 @@ void GANoteReader::resumeLecture()
 
 void GANoteReader::generatePartition()
 {
+    //remove and crate partiton file
     QFile::remove("..\\GitArc\\res\\partitions\\randomPartition.csv");
     QFile file("..\\GitArc\\res\\partitions\\randomPartition.csv");
     if(file.open(QIODevice::WriteOnly))
     {
-        QTextStream stream(&file);
+        QTextStream stream(&file);//use for read the file
+
+        //that create a partion of NB_NOTES rows and NUM_NOTES lines
         for(int i = 0; i < NB_NOTES; i++)
         {
             for(int j = 0; j < NUM_NOTES; j++)
             {
-                stream << QString::number(QRandomGenerator::global()->generate() % 2);
-                if(j != NUM_NOTES - 1)
+                stream << QString::number(QRandomGenerator::global()->generate() % 2);//generate number between 0 and 1
+                if(j != NUM_NOTES - 1)//put ';' symbol at the end of the number except for the last
                 {
                     stream << ";";
                 }
@@ -62,9 +66,7 @@ void GANoteReader::generatePartition()
             stream << endl;
         }
     }
-
     file.close();
-
 }
 
 void GANoteReader::readCSVNote()
--- GitArc/gashowrules.cpp
+++ GitArc/gashowrules.cpp
@@ -15,8 +15,12 @@ GAShowRules::GAShowRules(QWidget *parent) : QDialog(parent)
     this->setStyleSheet("QWidget {background-color:#0277BD; color:white;}");
     QString description = "Rules : \n"
                           "\t Press the key A, S, D and F to burn the note.\n"
-                          "\t Clic on this key when the note is on the the rectangle at the bottom oh the window.\n"
-                          "\t You can see your score and another informations on the right panel";
+                          "\t Clic on this key when the note are on the the rectangle at the bottom oh the window.\n"
+                          "\t You can see your score and another informations on the right panel. \n"
+                          "\t When you burn a note, if it become green, the note is burn and you win 100 points. \n"
+                          "\t if it become orange, the note is partially burn and you win a few points. \n"
+                          "\t if the horizontal note bar become red, you don't have burn the note and you lose 50 points. \n"
+                          "\t if the note go out of the screen without being burned, you lose 20 points.";
 
     QLabel *lblDesc = new QLabel(description, this);
     lblDesc->move(QPoint(20,20));
