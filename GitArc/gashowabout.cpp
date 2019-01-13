2e8f952579eb1bf949dc0daa6ff190377fe29ddf
jonas.freiburg <jonas.freiburghaus@he-arc.ch>
2019-01-13 14:29:34 +0100

    about and rules in txt file

--- GitArc/gashowabout.cpp
+++ GitArc/gashowabout.cpp
@@ -1,6 +1,7 @@
 #include "gashowabout.h"
 #include "constants.h"
 #include <QLabel>
+#include <QFile>
 
 /**
 * GAShowAbout
@@ -13,33 +14,18 @@ GAShowAbout::GAShowAbout(QWidget *parent) : QDialog(parent)
     this->setFixedSize(1000, 600);
     this->setModal(true);
     this->setStyleSheet("QWidget {background-color:#0277BD; color:white;}");
-    QString description = "Informations : \n"
-                          "\t École : He-Arc \n"
-                          "\t Cours : Projet P2 \n"
-                          "\t Professeurs : David Grunenwald, Stéphane Beuret \n"
-                          "\t Étudiants : Capocasale Romain, Freiburghaus Jonas and Moulin Vincent\n"
-                          "\t Annee scolaire : 2018 - 2019 \n\n\n"
-                          "Ressources : \n"
-                          "\t A$ap Ferg - New Level : \n"
-                          "\t\t License : Music Rights Management, Sony ATV Publishing, UMPG Publishing, EMI \n"
-                          "\t\t Lien : https://www.youtube.com/watch?v=Srns7NiO278\n"
-                          "\t Desiigner - Panda : \n"
-                          "\t\t License : Music Rights Management, CMRRA, Kobalt Music Publishing, AMRA, EMI  \n"
-                          "\t\t Lien : https://www.youtube.com/watch?v=4NJlUribp3c \n"
-                          "\t 50 Cent - P.I.M.P : \n"
-                          "\t\t License : Management, Kobalt Music Publishing, EMI Music Publishing, CMRRA, UBEM,   \n"
-                          "\t\t Lien : https://www.youtube.com/watch?v=UDApZhXTpH8 \n"
-                          "\t Snoop Dogg GIF : \n"
-                          "\t\t Lien : https://i.giphy.com/media/ZW1efT4aKTKg/giphy.webp \n"
-                          "\t ding.wav : \n"
-                          "\t\t License : Creative Common \n"
-                          "\t\t Lien : https://freesound.org/s/411749/ \n";
-                          "\t error.wav : \n"
-                          "\t\t License : Creative Common \n"
-                          "\t\t Lien : https://freesound.org/people/Autistic%20Lucario/sounds/142608/";
 
-    QLabel *lblDesc = new QLabel(description, this);
+    QString aboutText;
+    QString fileName(":/res/text/about.txt");
+
+    QFile file(fileName);
+    if(file.open(QIODevice::ReadOnly))
+    {
+        aboutText = file.readAll();
+        file.close();
+    }
+
+    QLabel *lblDesc = new QLabel(aboutText, this);
     lblDesc->move(QPoint(20,20));
     lblDesc->setFont(QFont(FONT, FONT_SIZE));
-
 }
--- GitArc/gashowrules.cpp
+++ GitArc/gashowrules.cpp
@@ -1,6 +1,7 @@
 #include "gashowrules.h"
 #include "constants.h"
 #include <QLabel>
+#include <QFile>
 
 /**
 * GAShowRules
@@ -13,23 +14,18 @@ GAShowRules::GAShowRules(QWidget *parent) : QDialog(parent)
     this->setFixedSize(1000, 600);
     this->setModal(true);
     this->setStyleSheet("QWidget {background-color:#0277BD; color:white;}");
-    QString description = "Rules : \n"
-<<<<<<< HEAD
-                          "\t Press the keys A, S, D and F to burn the note.\n"
-                          "\t Multiple keys can be pressed simultaneously. \n"
-                          "\t Click on the appopriate key when the note is in the the rectangle at the bottom of the window.\n"
-                          "\t You can see your score and some other informations on the right panel";
-=======
-                          "\t Press the key A, S, D and F to burn the note.\n"
-                          "\t Clic on this key when the note are on the the rectangle at the bottom oh the window.\n"
-                          "\t You can see your score and another informations on the right panel. \n"
-                          "\t When you burn a note, if it become green, the note is burn and you win 100 points. \n"
-                          "\t if it become orange, the note is partially burn and you win a few points. \n"
-                          "\t if the horizontal note bar become red, you don't have burn the note and you lose 50 points. \n"
-                          "\t if the note go out of the screen without being burned, you lose 20 points.";
->>>>>>> a7bdb0df9ccc77f2bce3c26b4c82c519d8d44b89
 
-    QLabel *lblDesc = new QLabel(description, this);
+    QString rulesText;
+    QString fileName(":/res/text/rules.txt");
+
+    QFile file(fileName);
+    if(file.open(QIODevice::ReadOnly))
+    {
+        rulesText = file.readAll();
+        file.close();
+    }
+
+    QLabel *lblDesc = new QLabel(rulesText, this);
     lblDesc->move(QPoint(20,20));
     lblDesc->setFont(QFont(FONT, FONT_SIZE));
 }
